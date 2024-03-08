5ca2ae35729a0ec8274385c28bd9c685542ed23d
Fernando Feliu Goncalez <fernando.goncalez@autotrac.com.br>
2024-03-08 10:53:45 -0300

    Atualizacao projeto

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 from flask import Flask, request
+from db import stores, items
+
 
 app = Flask(__name__)
-
-stores = [
-        {
-        "name": "My store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99            
-            } 
-            ]         
-        }
-    ]
 
 @app.get("/store")
 def get_stores():
